3c239707b640a2ad70244ec35ff1ec55e16152d3
millerdotkyle <kylenotfound@gmail.com>
2023-06-05 23:41:15 -0400

    changed comments
    -removed given comments describing starter code function
    -added new comments describing actual code function

--- assets/script.js
+++ assets/script.js
@@ -1,50 +1,9 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  // var parentID = $(this).parent().attr("id");
-
-
-
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  // var cTime = dayjs().format("H");
-  // console.log(typeof(cTime));
-  // $(".time-block").each(function () {
-  //   var parentID = $(this).parent().attr("id");
-  //   if(parentID.diff(currentHour) == 0) {
-  //     $(this).addClass("present");
-  //   }
-  // })
-
-
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this
-  // var timeBlocks = $(".time-block");
-  // console.log(timeBlocks);
-
-});
-
-// Starter code. 
-
-$(document).ready(function () {
+ $(document).ready(function () {
   $('.saveBtn').on('click', function () {
-    // get nearby values
+    // saved clicked btn parent id
     console.log($(this).parent().attr("id"));
     var selectedBtnParentID = $(this).parent().attr("id");
-    var savedTXT = $(selectedBtnParentID).children("textarea");
+    var savedTXT = $(selectedBtnParentID).children("textarea").val();
     console.log(savedTXT);
     localStorage.setItem(selectedBtnParentID, savedTXT);
   });
@@ -54,17 +13,17 @@ $(document).ready(function () {
   function hourUpdater() {
     var currentHour = dayjs().hour();
     console.log(currentHour);
-    // loop over time blocks
+    // jquery loop over time blocks
     $('.time-block').each(function () {
       var parentID = $(this).attr("id");
-      console.log(parentID);
+      // console.log(parentID);
+      //last 2 characters of id become the hour used to compare
       var parentHr = parentID.slice(5,7);
       // console.log(timeVar);
-      // var parentHr = dayjs().hour(timeVar);
-      console.log(parentHr);
-      console.log(currentHour);
+      // console.log(parentHr);
+      // console.log(currentHour);
 
-
+// evalueate time from dayjs vs value of the hour from the parent id
       if(currentHour == parentHr) {
         $(this).children("textarea").addClass("present");
     } else if (currentHour > parentHr) {
