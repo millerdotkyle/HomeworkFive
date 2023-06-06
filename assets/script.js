634ca844fae56484e0fab8861714802558b03dfb
millerdotkyle <kylenotfound@gmail.com>
2023-06-05 23:14:45 -0400

    changed time logic
    -changed time logic implementation
    -began get/set logic for plans text

--- assets/script.js
+++ assets/script.js
@@ -44,17 +44,12 @@ $(document).ready(function () {
     // get nearby values
     console.log($(this).parent().attr("id"));
     var selectedBtnParentID = $(this).parent().attr("id");
-    
+    var savedTXT = $(selectedBtnParentID).children("textarea");
+    console.log(savedTXT);
+    localStorage.setItem(selectedBtnParentID, savedTXT);
   });
 
-  var plans = JSON.parse(localStorage.getItem("storedPlans")) || [];
-  // if(plans.length === 0)
-  //   var hrs = ["hour-9", "hour-10", "hour-11", "hour-12", "hour-13", "hour-14", "hour-15", "hour-16", "hour-17"];
-  //   plans = {
-  //     hour: "hour-9",
-  //     plan: ""
-  //   }
-      
+
 
   function hourUpdater() {
     var currentHour = dayjs().hour();
@@ -62,23 +57,37 @@ $(document).ready(function () {
     // loop over time blocks
     $('.time-block').each(function () {
       var parentID = $(this).attr("id");
-      var intermediate = parentID.slice(-2);
-      // console.log(typeof(intermediate));
-      var parentHr = dayjs().hour(intermediate.value);
+      console.log(parentID);
+      var parentHr = parentID.slice(5,7);
+      // console.log(timeVar);
+      // var parentHr = dayjs().hour(timeVar);
       console.log(parentHr);
-      // console.log(parentHr.diff(currentHour));
-      if(dayjs().isSame(parentHr, "hour")) {
+      console.log(currentHour);
+
+
+      if(currentHour == parentHr) {
         $(this).children("textarea").addClass("present");
-    } else if (dayjs().isBefore(parentHr, "hour")) {
+    } else if (currentHour > parentHr) {
       $(this).children("textarea").addClass("past");
-    }else if (dayjs().isAfter(parentHr, "hour")){
+    }else if (currentHour < parentHr){
       $(this).children("textarea").addClass("future");
     }});
   }
+
+
   hourUpdater();
   setInterval(hourUpdater, 15000);
   // load any saved data from localStorage
   $('#hour-09 .description').val(localStorage.getItem('hour-09'));
+  $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+  $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+  $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+  $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+  $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+  $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+  $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+  $('#hour-17 .description').val(localStorage.getItem('hour-17'));
+
   // display current day on page
   $('#currentDay').text(dayjs().format('dddd, MMMM D, YYYY'));
 
@@ -87,3 +96,49 @@ $(document).ready(function () {
 
 
 
+
+
+
+
+
+//  var plans = JSON.parse(localStorage.getItem("storedPlans")) || [];
+// if(plans.length === 0)
+// plans = [
+// {
+//   hour: "09",
+//   plan: ""
+// },
+// {
+// hour: "10",
+// plan: ""
+// },
+// {
+//   hour: "11",
+//   plan: ""
+// },
+// {
+//   hour: "12",
+//   plan: ""
+// },
+// {
+//   hour: "13",
+//   plan: ""
+// },
+// {
+//   hour: "14",
+//   plan: ""
+// },
+// {
+//   hour: "15",
+//   plan: ""
+// },
+// {
+//   hour: "16",
+//   plan: ""
+// },
+// {
+//   hour: "17",
+//   plan: ""
+// }
+// ];
+// console.log(plans);
